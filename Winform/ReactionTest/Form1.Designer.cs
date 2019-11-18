99b099180263e68b5de47a8de46a5175df92780d
未闻 <ww@soye360.com>
2019-11-18 12:35:44 +0800

    Feat:新增桌面版本。

---
+++ Winform/ReactionTest/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace ReactionTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblGoal = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.picTop = new System.Windows.Forms.PictureBox();
+            this.picLeft = new System.Windows.Forms.PictureBox();
+            this.picRight = new System.Windows.Forms.PictureBox();
+            this.picBottom = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.菜单ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.tmrFPS = new System.Windows.Forms.Timer(this.components);
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblLevel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBottom)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "命中";
+            // 
+            // lblGoal
+            // 
+            this.lblGoal.AutoSize = true;
+            this.lblGoal.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblGoal.ForeColor = System.Drawing.Color.Red;
+            this.lblGoal.Location = new System.Drawing.Point(278, 34);
+            this.lblGoal.Name = "lblGoal";
+            this.lblGoal.Size = new System.Drawing.Size(20, 19);
+            this.lblGoal.TabIndex = 1;
+            this.lblGoal.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(323, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "错误";
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblError.ForeColor = System.Drawing.Color.Green;
+            this.lblError.Location = new System.Drawing.Point(358, 34);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(20, 19);
+            this.lblError.TabIndex = 1;
+            this.lblError.Text = "0";
+            // 
+            // picTop
+            // 
+            this.picTop.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picTop.BackgroundImage")));
+            this.picTop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picTop.Location = new System.Drawing.Point(274, 70);
+            this.picTop.Name = "picTop";
+            this.picTop.Size = new System.Drawing.Size(80, 80);
+            this.picTop.TabIndex = 2;
+            this.picTop.TabStop = false;
+            // 
+            // picLeft
+            // 
+            this.picLeft.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picLeft.BackgroundImage")));
+            this.picLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picLeft.Location = new System.Drawing.Point(172, 170);
+            this.picLeft.Name = "picLeft";
+            this.picLeft.Size = new System.Drawing.Size(80, 80);
+            this.picLeft.TabIndex = 2;
+            this.picLeft.TabStop = false;
+            // 
+            // picRight
+            // 
+            this.picRight.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picRight.BackgroundImage")));
+            this.picRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picRight.Location = new System.Drawing.Point(372, 170);
+            this.picRight.Name = "picRight";
+            this.picRight.Size = new System.Drawing.Size(80, 80);
+            this.picRight.TabIndex = 2;
+            this.picRight.TabStop = false;
+            // 
+            // picBottom
+            // 
+            this.picBottom.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picBottom.BackgroundImage")));
+            this.picBottom.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picBottom.Location = new System.Drawing.Point(274, 270);
+            this.picBottom.Name = "picBottom";
+            this.picBottom.Size = new System.Drawing.Size(80, 80);
+            this.picBottom.TabIndex = 2;
+            this.picBottom.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(9, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 70);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "* 操作指引 *\r\n\r\n空格键：开始or暂停\r\n   Esc：重置\r\n方向键：对应图形";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.菜单ToolStripMenuItem,
+            this.tsmiAbout});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(624, 25);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 菜单ToolStripMenuItem
+            // 
+            this.菜单ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiExit});
+            this.菜单ToolStripMenuItem.Name = "菜单ToolStripMenuItem";
+            this.菜单ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.菜单ToolStripMenuItem.Text = "菜单";
+            // 
+            // tsmiExit
+            // 
+            this.tsmiExit.Name = "tsmiExit";
+            this.tsmiExit.Size = new System.Drawing.Size(152, 22);
+            this.tsmiExit.Text = "退出";
+            this.tsmiExit.Click += new System.EventHandler(this.tsmiExit_Click);
+            // 
+            // tsmiAbout
+            // 
+            this.tsmiAbout.Name = "tsmiAbout";
+            this.tsmiAbout.Size = new System.Drawing.Size(44, 21);
+            this.tsmiAbout.Text = "关于";
+            this.tsmiAbout.Click += new System.EventHandler(this.tsmiAbout_Click);
+            // 
+            // tmrFPS
+            // 
+            this.tmrFPS.Interval = 1000;
+            this.tmrFPS.Tick += new System.EventHandler(this.tmrFPS_Tick);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblTotal.ForeColor = System.Drawing.Color.Gray;
+            this.lblTotal.Location = new System.Drawing.Point(492, 34);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(120, 48);
+            this.lblTotal.TabIndex = 1;
+            this.lblTotal.Text = "0000";
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblLevel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.lblLevel.Location = new System.Drawing.Point(188, 383);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(249, 29);
+            this.lblLevel.TabIndex = 1;
+            this.lblLevel.Text = "Lv：0  Speed：0";
+            this.lblLevel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 421);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picBottom);
+            this.Controls.Add(this.picRight);
+            this.Controls.Add(this.picLeft);
+            this.Controls.Add(this.picTop);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblLevel);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.lblGoal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "反应力测试";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBottom)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblGoal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.PictureBox picTop;
+        private System.Windows.Forms.PictureBox picLeft;
+        private System.Windows.Forms.PictureBox picRight;
+        private System.Windows.Forms.PictureBox picBottom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 菜单ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmiExit;
+        private System.Windows.Forms.ToolStripMenuItem tsmiAbout;
+        private System.Windows.Forms.Timer tmrFPS;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblLevel;
+    }
+}
+
---
+++ Winform/ReactionTest/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ReactionTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            speed = ConfigurationManager.AppSettings["speed"].Split(',').Select(x => Convert.ToInt32(x)).ToArray();
+            update = ConfigurationManager.AppSettings["update"].Split(',').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Reset();
+        }
+
+        private void tsmiExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void tsmiAbout_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://gitee.com/yokeqi/game_reaction");
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch (keyData)
+            {
+                case Keys.Escape:
+                    Reset();
+                    break;
+                case Keys.Space:
+                    StartOrPause();
+                    break;
+                case Keys.Left:
+                case Keys.Up:
+                case Keys.Right:
+                case Keys.Down:
+                    Tap((int)keyData - 37);
+                    break;
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private int goal = 0, error = 0;
+        private int level = 0;
+        private int[] speed;
+        private int[] update;
+        private void Reset()
+        {
+            goal = error = 0;
+            UpdateScore();
+
+            level = 0;
+            UpdateLevel();
+        }
+        private void StartOrPause()
+        {
+            if (tmrFPS.Enabled)
+                tmrFPS.Stop();
+            else
+                tmrFPS.Start();
+        }
+        private void Tap(int val)
+        {
+            if (_curFX == -1)
+                return;
+
+            if (val == _curFX)
+                goal++;
+            else
+                error++;
+
+            UpdateScore();
+        }
+
+
+        private Random _rnd = new Random(DateTime.Now.Millisecond);
+        private int _curFX = -1;
+        private void tmrFPS_Tick(object sender, EventArgs e)
+        {
+            picLeft.Visible = picTop.Visible = picRight.Visible = picBottom.Visible = false;
+            int val = -1;
+            do
+            {
+                val = _rnd.Next(0, 21) % 4;
+            } while (val == _curFX);
+            _curFX = val;
+            switch (_curFX)
+            {
+                case 0:
+                    picLeft.Visible = true;
+                    break;
+                case 1:
+                    picTop.Visible = true;
+                    break;
+                case 2:
+                    picRight.Visible = true;
+                    break;
+                case 3:
+                    picBottom.Visible = true;
+                    break;
+            }
+        }
+
+        private void UpdateScore()
+        {
+            lblGoal.Text = goal.ToString();
+            lblError.Text = error.ToString();
+            var total = goal - error;
+            lblTotal.Text = Math.Abs(total).ToString("0000");
+            lblTotal.ForeColor = (total == 0) ? Color.Gray : (total > 0) ? Color.Red : Color.Green;
+
+            if (total > update[level])
+            {
+                level++;
+                UpdateLevel();
+            }
+            else if (level > 0 && total < update[level - 1])
+            {
+                level--;
+                UpdateLevel();
+            }
+        }
+
+        private void UpdateLevel()
+        {
+            lblLevel.Text = $"Lv：{level + 1}，Speed：{speed[level]}";
+            tmrFPS.Interval = speed[level];
+        }
+    }
+}
